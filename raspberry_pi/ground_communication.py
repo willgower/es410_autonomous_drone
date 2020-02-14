8d0f6757363b623bc4f17e7c8800080854d0963c
willgower <willgower@hotmail.co.uk>
2020-02-14 19:49:06 +0000

    Created skeleton

--- raspberry_pi/ground_communication.py
+++ raspberry_pi/ground_communication.py
@@ -3,3 +3,20 @@
 # File: ground_communication.py
 # Description: Module to handle serial communication to the ground control station
 
+
+class GroundCommunication:
+    def __init__(self):
+        """
+        Start wireless serial connection to the GCS using the NRF24L01 modules.
+        Timeout - 10s
+        """
+
+    def read_message(self):
+        """
+        Read the latest message from the serial buffer and return it
+        """
+
+    def send_message(self):
+        """
+        Send the message back to the GCS
+        """
