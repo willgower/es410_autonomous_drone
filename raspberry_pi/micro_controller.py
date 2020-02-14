dd42c96aad34c13098cdae7d89776b397c25f379
bennett-j <53816688+bennett-j@users.noreply.github.com>
2020-02-14 19:02:26 +0000

    Skeleton for microcontroller link

--- raspberry_pi/micro_controller.py
+++ raspberry_pi/micro_controller.py
@@ -2,3 +2,35 @@
 # Owner: William Gower
 # File: micro_controller.py
 # Description: Module to handle serial connection to the Arduino
+
+class MicroController():
+    def __init__(self):
+        """
+        establish communication to arduino (timeout = 5 s)
+        get confirmation from arduino that grippers are open
+        """
+
+    def set_mode(self, mode):
+        """
+        send message to arduino to put into specified mode
+        mode:   0 - idle (arduino reads serial port awaiting instruction)
+                1 - current measuring
+                2 - enable parcel load (grippers closing)
+                3 - open grippers
+        """
+
+    def is_parcel_loaded(self):
+        """
+        non-blocking function to return true or false
+        """
+
+    def open_grippers(self):
+        """
+        first, set mode to 3
+        then read serial port in a loop until message that grippers are open
+        """
+
+    def get_current(self):
+        """
+        when called should return latest current reading
+        """
