7bb32dc0beb49cf7e429fec7f11a4ee65489df6f
willgower <willgower@hotmail.co.uk>
2020-02-14 17:38:07 +0000

    Update of flight controller methods

--- raspberry_pi/flight_controller.py
+++ raspberry_pi/flight_controller.py
@@ -4,3 +4,36 @@
 # Description: Module to handle MAVLink communication to the Pixhawk
 
 
+class FlightController:
+
+    def __init__(self):
+        """
+        Initialise module wide variables and objects.
+        """
+
+    def set_destination(self, location):
+        """
+        Takes in a string of a location from a predefined list.
+        E.g. "bluebell", "claycroft", "test_point_A", "test_point_B"
+        """
+
+    def land(self):
+        """
+        Land the drone in the exact position.
+        """
+
+    def set_battery_capacity(self, mah):
+        """
+        Set the size of the battery capacity in mAh.
+        """
+
+    def get_fc_status(self):
+        """
+        Return a json object containing all of the flight controller information
+        such as battery level, altitude, GPS, velocity.
+        """
+
+    def get_hwss_status(self):
+        """
+        Get the status of the hardware safety switch
+        """
