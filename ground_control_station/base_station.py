ace707003358ffcbefc84cd9b3682fa0887933dc
bennett-j <53816688+bennett-j@users.noreply.github.com>
2020-02-18 12:33:28 +0000

    Base station open connection to drone

--- ground_control_station/base_station.py
+++ ground_control_station/base_station.py
@@ -6,31 +6,22 @@
 from ground_control_station.drone_communication import DroneComms
 import sys
 
-class BaseStation:
-    def __init__(self):
-		 """
-		 start communcation with drone
-		 """
-		 
-		 self.drone = DroneComms()
-
-
-
-
-
 
 if __name__=="__main__":
 	while True:
 		# trying to connect to drone
-		try:
-			base = BaseStation()
-		except:
+		print("Trying to connect to drone...")
+        try:
+			drone = DroneComms()
+        except:
 			print("Failed to connect to drone.")
 			reponse = input("Would you like to try again? (y/n)")
 			if response == 'y':
 				continue
 			elif response == 'n':
 				break
+        else:
+            print("Connection established.")
 
 	# after while loop, exit program
 	sys.exit()
