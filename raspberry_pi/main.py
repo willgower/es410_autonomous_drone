fe4a55423add37f542d49caef515612188d673ea
willgower <willgower@hotmail.co.uk>
2020-02-21 16:46:19 +0000

    Changes for Friday testing

--- raspberry_pi/main.py
+++ raspberry_pi/main.py
@@ -3,12 +3,12 @@
 # File: main.py
 # Description: Module to be entry point and have overall control of companion computer operations
 
-from flight_controller import FlightController
-from ground_communication import GroundControlStation
-from micro_controller import MicroController
-from data_logging import DataLogging
-from landing_vision import LandingVision
-from recurring_timer import RecurringTimer
+from raspberry_pi.flight_controller import FlightController
+from raspberry_pi.ground_communication import GroundControlStation
+from raspberry_pi.micro_controller import MicroController
+from raspberry_pi.data_logging import DataLogging
+from raspberry_pi.landing_vision import LandingVision
+from raspberry_pi.recurring_timer import RecurringTimer
 
 import sys
 import json
