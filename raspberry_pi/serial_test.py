9792144b9da42f3c3132d7e8b512e087650c0ebf
willgower <willgower@hotmail.co.uk>
2020-02-24 18:00:34 +0000

    HC-12 Serial test

--- raspberry_pi/serial_test.py
+++ raspberry_pi/serial_test.py
@@ -7,4 +7,11 @@ counter = 0
 
 while True:
     wireless.write("Does this work?".encode('utf-8'))
+    print("sent")
+
+    r = wireless.read(100)
+
+    if r:
+        print(r)
+
     time.sleep(1)
