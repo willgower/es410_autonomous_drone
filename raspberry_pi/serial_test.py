f6b61409dba6e810782939826525e9a5391994fe
willgower <willgower@hotmail.co.uk>
2020-02-24 17:57:51 +0000

    HC-12 Serial test

--- raspberry_pi/serial_test.py
+++ raspberry_pi/serial_test.py
@@ -6,5 +6,5 @@ wireless = serial.Serial(port='/dev/ttyAMA0', baudrate=9600)
 counter = 0
 
 while True:
-    wireless.write("Does this work?")
+    wireless.write("Does this work?".encode('utf-8'))
     time.sleep(1)
