241c5374613125ece1b1a7887be2261879c8900e
willgower <willgower@hotmail.co.uk>
2020-02-24 18:04:58 +0000

    HC-12 Serial test

--- raspberry_pi/serial_test.py
+++ raspberry_pi/serial_test.py
@@ -1,5 +1,9 @@
 import wiringpi
+import time
 
 wiringpi.wiringPiSetup()
 serial = wiringpi.serialOpen('/dev/ttyAMA0', 9600)
-wiringpi.serialPuts(serial, 'hello world!')
+
+while True:
+    wiringpi.serialPuts(serial, 'hello world!')
+    time.sleep(1)
