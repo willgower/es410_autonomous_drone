a64994154309511071b5eed150f11a2070a1f52a
willgower <willgower@hotmail.co.uk>
2020-02-24 18:49:38 +0000

    HC-12 Serial test

--- raspberry_pi/serial_test.py
+++ raspberry_pi/serial_test.py
@@ -4,6 +4,6 @@ import time
 wireless = serial.Serial('/dev/ttyS0', 9600)
 
 while True:
-    wireless.write('hello world!\n')
+    wireless.write("hello world!\n".encode('utf-8'))
     print("sent")
     time.sleep(1)
