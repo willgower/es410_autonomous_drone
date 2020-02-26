4c6e84c241acd4b3b973ddb62e78640951c7043b
willgower <willgower@hotmail.co.uk>
2020-02-26 01:05:34 +0000

    Started to tidy and prepare for RPi

--- raspberry_pi/landing_vision.py
+++ raspberry_pi/landing_vision.py
@@ -9,13 +9,13 @@
 # → bare bones completed by JRB so DroneControl can be written
 # → Aaron, you need to specify what input arguments you want to
 #   the get_instruction() method
+# → Formatting and edit for use on RPi by Will
 # =====================================
 
 from collections import deque
-import numpy as np
-import argparse
 import imutils
 import cv2
+
 
 class LandingVision:
     def __init__(self):
@@ -23,39 +23,40 @@ class LandingVision:
         over to you Aaron...
         """
 
+    def sift_detector(self, new_image, image_template):
+        # Function that compares input image to template
+        # It then returns the number of SIFT matches between them
+        image1 = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
+        image2 = image_template
+
+        # Create SIFT detector object
+        sift = cv2.xfeatures2d.SIFT_create()
+
+        # Obtain the keypoints and descriptors using SIFT
+        keypoints_1, descriptors_1 = sift.detectAndCompute(image1, None)
+        keypoints_2, descriptors_2 = sift.detectAndCompute(image2, None)
+
+        # Define parameters for our Flann Matcher
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=3)
+        search_params = dict(checks=100)
+
+        # Create the Flann Matcher object
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        # Obtain matches using K-Nearest Neighbor Method
+        # the result 'matchs' is the number of similar matches found in both images
+        matches = flann.knnMatch(descriptors_1, descriptors_2, k=2)
+
+        # Store good matches using Lowe's ratio test
+        good_matches = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+
+        return len(good_matches)
+
     def get_instruction(self, altitude):
-        def sift_detector(new_image, image_template):
-            # Function that compares input image to template
-            # It then returns the number of SIFT matches between them
-            image1 = cv2.cvtColor(new_image, cv2.COLOR_BGR2GRAY)
-            image2 = image_template
-
-            # Create SIFT detector object
-            #sift = cv2.SIFT()
-            sift = cv2.xfeatures2d.SIFT_create()
-            # Obtain the keypoints and descriptors using SIFT
-            keypoints_1, descriptors_1 = sift.detectAndCompute(image1, None)
-            keypoints_2, descriptors_2 = sift.detectAndCompute(image2, None)
-
-            # Define parameters for our Flann Matcher
-            FLANN_INDEX_KDTREE = 0
-            index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 3)
-            search_params = dict(checks = 100)
-
-            # Create the Flann Matcher object
-            flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-            # Obtain matches using K-Nearest Neighbor Method
-            # the result 'matchs' is the number of similar matches found in both images
-            matches = flann.knnMatch(descriptors_1, descriptors_2, k=2)
-
-            # Store good matches using Lowe's ratio test
-            good_matches = []
-            for m,n in matches:
-                if m.distance < 0.7 * n.distance:
-                    good_matches.append(m) 
-            return len(good_matches)
-
         cap = cv2.VideoCapture(0)
 
         # Load our image template, this is our reference image - the drone logo
