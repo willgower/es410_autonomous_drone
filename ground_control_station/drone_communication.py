5e29bab586b8ab64cef35212be8f64d0c1ace591
willgower <willgower@hotmail.co.uk>
2020-03-04 16:12:11 +0000

    Added SITL testing functionality

--- ground_control_station/drone_communication.py
+++ ground_control_station/drone_communication.py
@@ -17,7 +17,8 @@ class DroneComms:
 		if socket.gethostname() == "william-XPS-13-9360":
 			# Start a serial connection with Will's laptop
 			self.ser = serial.Serial(
-				"/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0",
+				# "/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0",
+				'/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_55739323437351811190-if00',
 				baudrate=9600,
 				timeout=0.05)
 		elif socket.gethostname() == "JAMESHPLAPTOP":
@@ -33,6 +34,7 @@ class DroneComms:
 
 		# Start handshake procedure
 		handshake_complete = False
+		print("HC-12 Connected - waiting for message from drone")
 
 		while not handshake_complete:
 			time.sleep(0.5)
